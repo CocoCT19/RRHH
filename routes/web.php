50d65b991e4ea1144560888f3b31e743d88febc3
Luis Guerra <lguerrariobo@gmail.com>
2026-03-24 17:41:10 -0500

    feat(Collaborators): implementa CRUD de colaboradores con TDD (CP-001)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CollaboratorController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('collaborators')->group(function () {
+
+    Route::get('/', [CollaboratorController::class,'index']);
+
+    Route::post('/', [CollaboratorController::class,'store']);
+
+    Route::put('/{id}', [CollaboratorController::class,'update']);
+
+    Route::delete('/{id}', [CollaboratorController::class,'destroy']);
+
 });
